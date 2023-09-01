279f18ce9e1ccf9a8ae289fa2b97e8966397709e
Lucas <lcs.wgnr@posteo.de>
2023-09-01 17:27:10 +0200

    added a moving_average

--- envs/assets/env_utilities.py
+++ envs/assets/env_utilities.py
@@ -105,3 +105,14 @@ def get_model_names():
         if file.endswith('.zip'):
             model_names.append(file)
     return model_names
+
+
+def moving_average(data, window_size):
+    """
+    Calculate a moving average over 1D data using a specified window size.
+
+    :param data: The data to calculate the moving average over.
+    :param window_size: The window size to use for the moving average.
+    :return: The moving average of the data.
+    """
+    return np.convolve(data, np.ones(window_size) / window_size, mode='valid')
