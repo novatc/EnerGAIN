71187ed5e6bd556954cec2fc745b8d0fb09cf5af
Lucas <lcs.wgnr@posteo.de>
2023-09-06 11:17:51 +0200

    dummy_agent.py works with unscaled data

--- dummy_agent.py
+++ dummy_agent.py
@@ -9,12 +9,18 @@ class TradingBot:
         self.buy_threshold = buy_threshold
         self.sell_threshold = sell_threshold
         self.successful_trades = []
-        self.current_charge = 0.5
-        self.max_amount = 1
+        self.current_charge = 500
+        self.max_amount = 1000
         self.min_amount = 0
         self.profit = 0
 
     def check_constrains(self, intended_amount, trade_intend) -> bool:
+        """
+        Check if the bot can trade the intended amount
+        :param intended_amount: the bot intends to trade
+        :param trade_intend: the bot intends to buy or sell
+        :return:
+        """
         if trade_intend == 'sell' and intended_amount > self.current_charge:
             return False
         if trade_intend == 'buy' and intended_amount + self.current_charge > self.max_amount:
@@ -29,7 +35,7 @@ class TradingBot:
         if current_price < self.buy_threshold:
             intent = 'buy'
             current_price += 0.01
-            buy_amount = 0.5
+            buy_amount = 500
             if self.check_constrains(buy_amount, trade_intend='buy'):
                 if self.market.accept_offer(current_price, intent):
                     self.current_charge += abs(buy_amount)
@@ -39,7 +45,7 @@ class TradingBot:
         if current_price > self.sell_threshold:
             intent = 'sell'
             current_price -= 0.01
-            sell_amount = 0.5
+            sell_amount = 500
             # make sure the bot doesn't sell more than it has
             if self.check_constrains(sell_amount, trade_intend='sell'):
                 if self.market.accept_offer(current_price, 'sell'):
@@ -86,10 +92,16 @@ class TradingBot:
 
 
 # Initialize the Market and the bot
-sell_threshold = -0.70
-buy_threshold = -0.78
+#     Mean Price: ≈0.195
+#     Median Price: ≈0.167
+#     Standard Deviation: ≈0.123
+#     Minimum Price: ≈−0.004
+#     Maximum Price: ≈0.665
 
-data = pd.read_csv("data/clean/test_set.csv")
+sell_threshold = 0.318
+buy_threshold = 0.0720
+
+data = pd.read_csv("data/in-use/unscaled_eval_data.csv")
 market = Market(data)
 bot = TradingBot(market, sell_threshold=sell_threshold, buy_threshold=buy_threshold)
 
