25ce5f93dbd28277f482936ebc073834bfded4b7
Lucas <lcs.wgnr@posteo.de>
2023-09-29 11:14:13 +0200

    preprocessed the prl data

--- preprocess_prl.py
+++ preprocess_prl.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 prl = pd.read_csv('data/prm/prl.csv', index_col=0, sep=';')
@@ -20,6 +21,31 @@ prl['price'] = prl['price'] / 1000  # convert to €/kWh
 prl['amount'] = prl['amount'].interpolate(method='linear')
 
 print(prl.isnull().sum())
+
+# save the number of the day of the week in a new column
+prl['day_of_week'] = prl.index.dayofweek
+# save the number of the month in a new column
+prl['month'] = prl.index.month
+# for prl['hour'] use prl.start and convert to float
+prl['hour'] = prl['start'].str.split(':').str[0]
+prl['hour'] = pd.to_numeric(prl['hour'], errors='coerce')
+
+# Apply cyclical encoding using sine and cosine transformations
+prl['hour_sin'] = np.sin(2 * np.pi * prl['hour'] / 24)
+prl['hour_cos'] = np.cos(2 * np.pi * prl['hour'] / 24)
+
+prl['day_of_week_sin'] = np.sin(2 * np.pi * prl['day_of_week'] / 7)
+prl['day_of_week_cos'] = np.cos(2 * np.pi * prl['day_of_week'] / 7)
+
+prl['month_sin'] = np.sin(2 * np.pi * prl['month'] / 12)
+prl['month_cos'] = np.cos(2 * np.pi * prl['month'] / 12)
+
+# Drop the "day_of_week, month, hour" columns
+prl = prl.drop(['day_of_week', 'month', 'hour', 'start', 'end',], axis=1)
+
+# make price the index
+prl = prl.set_index('price')
+
 
 # Save the preprocessed data to a csv file
 prl.to_csv('data/prm/preprocessed_prl.csv')
