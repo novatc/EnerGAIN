9909b1c9ce930edb6e75df07008624c7f80437a9
Lucas Wagner <l.wagner@lintas-greenenergy.de>
2023-11-24 14:11:48 +0100

    trend env with new structure

--- envs/trend_env.py
+++ envs/trend_env.py
@@ -12,12 +12,12 @@ from envs.assets.plot_engien import plot_reward, plot_savings, plot_charge, plot
 class TrendEnv(gym.Env):
     def __init__(self, da_data_path, validation=False):
         super(TrendEnv, self).__init__()
-        self.dataframe = pd.read_csv(da_data_path)
+        self.da_dataframe = pd.read_csv(da_data_path)
         self.trend_horizon = 4
 
-        low_boundary = self.dataframe.min().values
+        low_boundary = self.da_dataframe.min().values
 
-        high_boundary = self.dataframe.max().values
+        high_boundary = self.da_dataframe.max().values
 
         low_boundary = np.tile(low_boundary, self.trend_horizon)  # repeat the array 4 times to match the obs space
         high_boundary = np.tile(high_boundary, self.trend_horizon)
@@ -25,14 +25,14 @@ class TrendEnv(gym.Env):
         # add 10 to each value in the high boundary to make sure the agent can't reach the upper boundary
         high_boundary += 10
 
-        action_low = np.array([-1.0, -500.0])
-        action_high = np.array([1.0, 500.0])
+        action_low = np.array([-1.0, 1000.0])
+        action_high = np.array([1.0, 1000.0])
 
         self.action_space = spaces.Box(low=action_low, high=action_high, shape=(2,), dtype=np.float32)
         self.observation_space = spaces.Box(low=-low_boundary, high=high_boundary,
-                                            shape=((self.dataframe.shape[1]) * self.trend_horizon,))
+                                            shape=((self.da_dataframe.shape[1]) * self.trend_horizon,))
 
-        self.day_ahead = DayAhead(self.dataframe)
+        self.day_ahead = DayAhead(self.da_dataframe)
         self.savings = 50  # €
         self.battery = Battery(1000, 500)
         self.battery.add_charge_log(self.battery.get_soc())
@@ -46,22 +46,35 @@ class TrendEnv(gym.Env):
 
         self.rewards = []
         self.reward_log = []
-        self.window_size = 20
-        self.penalty = -5
+        self.window_size = 5
+        self.penalty = -30
+
+        self.trade_threshold = 10
 
         self.validation = validation
 
     def step(self, action):
+        should_truncated = False
         if self.validation:
             self.day_ahead.step()
         else:
-            self.day_ahead.random_walk()
+            should_truncated = self.day_ahead.random_walk(24 * 7)
 
-        price = action[0].item()
-        amount = action[1].item()
+        price, amount = action
+
+        reward = 0
 
         terminated = False  # Whether the agent reaches the terminal state
-        truncated = False  # this can be Fasle all the time since there is no failure condition the agent could trigger
+        truncated = should_truncated  # this can be Fasle all the time since there is no failure condition the agent could trigger
+
+        # Handle DA trade or holding
+        if -self.trade_threshold < amount < self.trade_threshold:
+            reward += self.handle_holding()
+
+        reward += self.perform_da_trade(energy_amount=amount, market_price=price)
+
+        self.reward_log.append(
+            (self.reward_log[-1] + reward) if self.reward_log else reward)  # to keep track of the reward over time
         info = {'current_price': self.day_ahead.get_current_price(),
                 'current_step': self.day_ahead.get_current_step(),
                 'savings': self.savings,
@@ -69,20 +82,43 @@ class TrendEnv(gym.Env):
                 'action_price': price,
                 'action_amount': amount,
                 }
-
-        if amount > 0:  # buy
-            reward = self.trade(price, amount, 'buy')
-        elif amount < 0:  # sell
-            reward = self.trade(price, amount, 'sell')
-        else:  # if amount is 0
-            reward = 0
-            self.holding.append((self.day_ahead.get_current_step(), 'hold'))
-
-        self.rewards.append(reward)
-        self.reward_log.append(
-            (self.reward_log[-1] + reward) if self.reward_log else reward)  # to keep track of the reward over time
         # Return the current state of the environment as a numpy array, the reward,
         return self.get_observation().astype(np.float32), reward, terminated, truncated, info
+
+    def is_trade_valid(self, price, amount, trade_type):
+        """
+        Check if a trade is valid, i.e. if the battery can handle the trade and if the agent has enough savings.
+
+        :param price: (float) The price at which the trade is attempted.
+        :param amount: (float) The amount of energy to be traded. Positive values indicate buying or charging,
+                       and negative values indicate selling or discharging.
+        :param trade_type: (str) Type of trade to execute, accepted values are 'buy' or 'sell'.
+
+        :return: (bool) True if the trade is valid, False otherwise.
+        """
+        if trade_type == 'buy':
+            if price * amount > self.savings or self.savings <= 0 or self.battery.can_charge(amount) is False:
+                self.log_trades(False, 'buy', price, amount, self.penalty,
+                                'savings' if self.savings <= 0 else 'battery')
+                return False
+        elif trade_type == 'sell':
+            if self.battery.can_discharge(amount) is False:
+                self.log_trades(False, 'sell', price, amount, self.penalty, 'battery')
+                return False
+        else:
+            raise ValueError(f"Invalid trade type: {trade_type}")
+
+        return True
+
+    def perform_da_trade(self, energy_amount: float, market_price: float) -> float:
+        """
+        Perform a trade on the day-ahead market.
+
+        :param energy_amount: Energy to be traded (positive for buying, negative for selling).
+        :param market_price: Price at which the trade is attempted.
+        :return: Reward based on the trade outcome.
+        """
+        return self.trade(market_price, energy_amount, 'buy' if energy_amount > 0 else 'sell')
 
     def trade(self, price, amount, trade_type):
         """
@@ -98,30 +134,51 @@ class TrendEnv(gym.Env):
 
         :raises ValueError: If `trade_type` is neither 'buy' nor 'sell'.
         """
-        if trade_type == 'buy':
-            if price * amount > self.savings or self.savings <= 0 or self.battery.can_charge(amount) is False:
-                return self.penalty
-        elif trade_type == 'sell':
-            if self.battery.can_discharge(amount) is False:
-                return self.penalty
-        else:
+        if trade_type not in ['buy', 'sell']:
             raise ValueError(f"Invalid trade type: {trade_type}")
-        if self.day_ahead.accept_offer(price, trade_type):
-            # this works for both buy and sell because amount is negative for sale and + and - cancel out and fot buy
-            # amount is positive
-            self.battery.charge(amount)
-            # the same applies here for savings
-            self.savings -= self.day_ahead.get_current_price() * amount
-            self.battery.add_charge_log(self.battery.get_soc())
-            self.savings_log.append(self.savings)
-            self.trade_log.append((self.day_ahead.get_current_step(), price, amount, trade_type,
-                                   abs(float(self.day_ahead.get_current_price()) * amount)))
-        else:
-            self.invalid_trades.append((self.day_ahead.get_current_step(), price, amount, trade_type,
-                                        abs(float(self.day_ahead.get_current_price()) * amount)))
+
+        # Check if the trade is valid
+        if not self.is_trade_valid(price, amount, trade_type):
             return self.penalty
 
-        return abs(float(self.day_ahead.get_current_price()) * amount)
+        return self.execute_trade(price, amount, trade_type)
+
+    def execute_trade(self, price, amount, trade_type):
+        """
+        Execute the validated trade and update the system status.
+
+        :param price: Price at which the trade is executed.
+        :param amount: Amount of energy involved in the trade.
+        :param trade_type: 'buy' or 'sell'.
+        :return: Profit (or loss) from the trade.
+        """
+        current_price = self.day_ahead.get_current_price()
+        profit = 0
+        if self.day_ahead.accept_offer(price, trade_type):
+            if trade_type == 'buy':
+                self.battery.charge(amount)  # Charge battery for buy trades
+                self.savings -= current_price * amount  # Update savings
+                profit = -current_price * amount  # Negative profit for buying
+
+            elif trade_type == 'sell':
+                self.battery.charge(amount)  # Discharge battery for sell trades
+                self.savings += current_price * abs(amount)  # Update savings
+                profit = current_price * abs(amount)  # Positive profit for selling
+        else:
+            self.log_trades(False, trade_type, price, amount, self.penalty, 'market rejected')
+            return self.penalty
+
+        # Logging the trade details
+        self.battery.add_charge_log(self.battery.get_soc())
+        self.savings_log.append(self.savings)
+        self.log_trades(True, trade_type, price, amount, profit, 'accepted')
+
+        return profit
+
+    def handle_holding(self):
+        # Logic for handling the holding scenario
+        self.holding.append((self.day_ahead.get_current_step(), 'hold'))
+        return 5
 
     def get_observation(self):
         # Return the current state of the environment as a numpy array
@@ -150,11 +207,11 @@ class TrendEnv(gym.Env):
         plot_savings(self.savings_log, self.window_size, 'trend')
         plot_charge(self.window_size, self.battery, 'trend')
         plot_trades_timeline(trade_source=self.trade_log, title='Trades', buy_color='green', sell_color='red',
-                             model_name='trend')
+                             model_name='trend', data=self.da_dataframe)
         plot_trades_timeline(trade_source=self.invalid_trades, title='Invalid Trades', buy_color='black',
-                             sell_color='brown', model_name='trend')
-        plot_holding(self.holding, 'trend')
-        kernel_density_estimation(self.trade_log)
+                             sell_color='brown', model_name='trend', data=self.da_dataframe)
+        plot_holding(self.holding, 'trend', da_data=self.da_dataframe)
+        kernel_density_estimation(self.trade_log, model_name='trend', da_data=self.da_dataframe)
 
     def get_trades(self):
         """
@@ -162,3 +219,27 @@ class TrendEnv(gym.Env):
         :return: the trade log as a list of tuples
         """
         return self.trade_log
+
+    def get_invalid_trades(self):
+        return self.invalid_trades
+
+    def log_trades(self, valid: bool, type: str, offered_price: float, amount: float, reward: float,
+                   case: str) -> None:
+        """
+        Log the trades
+        :param type: the trade type
+        :param valid: if the trade was accepted or not
+        :param offered_price: the offered price
+        :param amount: the trade amount
+        :param reward: the reward based on the offer
+        :param case: if not valid, why
+        :return: None
+        """
+        if valid:
+            self.trade_log.append(
+                (self.day_ahead.get_current_step(), type, self.day_ahead.get_current_price(), offered_price, amount,
+                 reward, case))
+        else:
+            self.invalid_trades.append(
+                (self.day_ahead.get_current_step(), type, self.day_ahead.get_current_price(), offered_price, amount,
+                 reward, case))
