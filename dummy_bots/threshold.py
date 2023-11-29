79073e706678f90821d6d64435491c39cfbf38e5
Lucas <lcs.wgnr@posteo.de>
2023-11-29 19:54:33 +0100

    Update SimpleThresholdBot parameters and trade log format
    Updated the SimpleThresholdBot's initial parameters, specifically initial_inventory and unit_buy_sell. The trade log's format has been modified for more accurate logging and the buying/selling decision making process in the trade method has been tweaked for better trading results. Also, implemented a basic statistical analysis of the price data within the dummy bot script.

--- dummy_bots/threshold.py
+++ dummy_bots/threshold.py
@@ -6,7 +6,7 @@ from envs.assets.dayahead import DayAhead
 
 class SimpleThresholdBot:
     def __init__(self, market_sim, buy_threshold, sell_threshold, initial_money=50.0,
-                 initial_inventory=0, max_inventory=1000, unit_buy_sell=500):
+                 initial_inventory=500, max_inventory=1000, unit_buy_sell=50):
         "Initialize the SimpleThresholdBot with given parameters."
         self.market = market_sim
         self.money = initial_money
@@ -20,8 +20,8 @@ class SimpleThresholdBot:
         self.trade_log = []
 
     def log_trade(self, step, offer_price, offer_amount, trade_type):
-        self.trade_log.append((step, offer_price, offer_amount, trade_type,
-                               abs(float(self.market.get_current_price()) * offer_amount)))
+        self.trade_log.append((step, trade_type, self.market.get_current_price(), offer_price, offer_amount,
+                               abs(float(offer_price) * offer_amount)))
 
     def trade(self):
         "Perform a trade based on the set thresholds."
@@ -29,7 +29,8 @@ class SimpleThresholdBot:
         current_price = self.market.get_current_price()
 
         # Check if we should buy
-        if current_price <= self.buy_threshold and self.money >= self.unit_buy_sell * current_price:
+        if (current_price <= self.buy_threshold and self.money >= self.unit_buy_sell * current_price
+                and self.inventory + 100 < self.max_inventory):
             adjusted_buy_price = current_price * 1.1  # 10% higher to ensure market acceptance
             if self.market.accept_offer(adjusted_buy_price, 'buy'):
                 self.money -= self.unit_buy_sell * current_price  # Pay the actual current price
@@ -38,15 +39,15 @@ class SimpleThresholdBot:
                     excess_units = self.inventory - self.max_inventory
                     self.inventory = self.max_inventory
                     self.money += excess_units * current_price
-                self.log_trade(step, current_price, self.unit_buy_sell, 'buy')
+                self.log_trade(step, adjusted_buy_price, self.unit_buy_sell, 'buy')
 
         # Check if we should sell
-        elif current_price >= self.sell_threshold and self.inventory >= self.unit_buy_sell:
+        elif current_price >= self.sell_threshold and self.inventory >= self.unit_buy_sell and self.inventory - 100 > 0:
             adjusted_sell_price = current_price * 0.9  # 10% lower to ensure market acceptance
             if self.market.accept_offer(adjusted_sell_price, 'sell'):
                 self.money += self.unit_buy_sell * current_price  # Receive the actual current price
                 self.inventory -= self.unit_buy_sell
-                self.log_trade(step, current_price, self.unit_buy_sell, 'sell')
+                self.log_trade(step, adjusted_sell_price, self.unit_buy_sell, 'sell')
 
         # Record the state for this time
         self.money_over_time.append(self.money)
@@ -64,16 +65,29 @@ class SimpleThresholdBot:
 
 
 # Example usage
-#print current working directory
-import os
-print(os.getcwd())
+market = DayAhead(pd.read_csv('../data/in-use/average_da_year.csv'))
 
-market = DayAhead(pd.read_csv('data/in-use/unscaled_eval_data.csv'))
-bot = SimpleThresholdBot(market, buy_threshold=10.0, sell_threshold=15.0, initial_money=50.0,
-                         initial_inventory=500, max_inventory=1000, unit_buy_sell=50)
+# Load your dataset
+file_path = '../data/in-use/average_da_year.csv'
+data = pd.read_csv(file_path)
+
+# Basic statistical analysis of the price data
+price_stats = data['price'].describe()
+
+# Calculate additional statistics like variance and specific percentiles
+price_variance = data['price'].var()
+percentiles = data['price'].quantile([0.25, 0.5, 0.75])
+
+# Compile and print the statistics
+stats_summary = pd.concat([price_stats, pd.Series(price_variance, index=['variance']), percentiles])
+print(stats_summary)
+
+bot = SimpleThresholdBot(market, buy_threshold=0.047847, sell_threshold=0.226536, initial_money=50.0,
+                         initial_inventory=500, max_inventory=1000, unit_buy_sell=10)
 money, inventory = bot.run_simulation()
 print(f"Money: {money}, Inventory: {inventory}")
 
 trades = bot.get_trades()
-trades_log = pd.DataFrame(trades, columns=["step", "price", "amount", "trade_type", "reward"])
-trades_log.to_csv(f"trade_logs/simple_threshold_bot_trades.csv", index=False)
+trades_log = pd.DataFrame(trades,
+                          columns=["step", "type", "market price", "offered_price", "amount", "reward"])
+trades_log.to_csv("../trade_logs/simple_threshold_bot_trades.csv", index=False)
